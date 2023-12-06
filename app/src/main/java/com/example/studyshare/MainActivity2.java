7513d73f2d87b72e51427322029963832db7c9ec
EmmanuelTejedaTejeda <emmanuel.2d.28@gmail.com>
2023-12-06 10:15:16 -0600

    prueba4

--- app/src/main/java/com/example/studyshare/MainActivity2.java
+++ app/src/main/java/com/example/studyshare/MainActivity2.java
@@ -2,42 +2,78 @@ package com.example.studyshare;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.NavHost;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-
+    TextView cambiodefragmento;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Button toolbar;
+
+    MenuItem face, insta, mdl;
     private boolean doubleBackToExitPressedOnce = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        // Obtén el NavController desde el NavHostFragment
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination navDestination, @Nullable Bundle bundle) {
+                // Obtén el ID del fragmento actual
+                int currentDestinationId = navController.getCurrentDestination().getId();
+                cambiodefragmento = findViewById(R.id.cambiodefragment);
+                // Actualiza el texto de la cabecera según el fragmento actual
+                if (currentDestinationId == R.id.inicio) {
+                    cambiodefragmento.setText("Inicio");
+                    cambiodefragmento.setTextColor(getColor(R.color.white));
+                } else if (currentDestinationId == R.id.files) {
+                    cambiodefragmento.setText("Archivos");
+                    cambiodefragmento.setTextColor(getColor(R.color.white));
+                } else if (currentDestinationId == R.id.user) {
+                    cambiodefragmento.setText("Usuario");
+                    cambiodefragmento.setTextColor(getColor(R.color.white));
+                }
+            }
+        });
+
         //Lateral menu bar
         drawerLayout =  findViewById(R.id.drawerLayout);
         navigationView =  findViewById(R.id.menureal);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
         toolbar = findViewById(R.id.menu);
         toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,14 +106,25 @@ public class MainActivity2 extends AppCompatActivity {
         };
         getOnBackPressedDispatcher().addCallback(this, callback);
         navegar();
-    }
 
+    }
     private void navegar(){
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.botones);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
     }
 
     public void cerrarSesion(MenuItem item) {
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.fak:
+                Toast.makeText(this, "sexooooooooo", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
